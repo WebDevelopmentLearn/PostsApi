4a8d96cb6638aac033bf7c8c7a6e7c5b2f0c9dbb
007killer2 <savenko.artem@gmx.de>
2024-11-23 23:48:34 +0100

    Minor edits

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -14,7 +14,6 @@ function App() {
   return (
     <div className="App">
         <Navbar />
-        <h1>Posts API</h1>
         <MainRoute/>
     </div>
   );
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,10 +11,13 @@ export const Navbar = () => {
 
     return (
         <div className={styles.Navbar}>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/register">Register</NavLink>
-            {isAuthenticated && <NavLink to="/profile">Profile</NavLink>}
+            <h3>Posts API</h3>
+            <div className={styles.LinksList}>
+                <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/">Home</NavLink>
+                <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/login">Login</NavLink>
+                <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/register">Register</NavLink>
+                {isAuthenticated && <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/profile">Profile</NavLink>}
+            </div>
         </div>
     )
 }
