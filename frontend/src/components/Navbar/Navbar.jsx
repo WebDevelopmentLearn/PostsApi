c37ceb126bcc8e9d884a31838d5f99ec600612d8
007killer2 <savenko.artem@gmx.de>
2024-12-04 12:51:24 +0100

    Some changes in Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import {NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import styles from "./Navbar.module.scss";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {API_URL, logout} from "../../store/reducers/actionCreators";
 export const Navbar = () => {
     const {isAuthenticated, user} = useSelector((state) => state.authReducer);
-
+    const dispatch = useDispatch();
+    const handleLogout = () => {
+        dispatch(logout());
+    }
     return (
         <div className={styles.Navbar}>
             <h3>Posts API</h3>
@@ -11,9 +15,12 @@ export const Navbar = () => {
                 <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/">Home</NavLink>
                 {!isAuthenticated && <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/login">Login</NavLink>}
                 {!isAuthenticated && <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/register">Register</NavLink>}
-                {isAuthenticated && <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/profile">Profile</NavLink>}
+                {/*{isAuthenticated && <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/profile">Profile</NavLink>}*/}
                 {(isAuthenticated && user?.role === "admin") && <NavLink className={({isActive}) => isActive ? styles.isActive : ""} to="/users">Users List</NavLink>}
             </div>
+            {isAuthenticated && <Link className={styles.ProfileLink} to={"/profile"}>
+                <img src={`${API_URL}${user?.avatar}`} alt="user_avatar"/>
+            </Link>}
         </div>
     )
 }
